d33f29bc6938a7de6c5a304ee5765011702ecf3c
Vishal Sathyanarayana <vishal23dps@gmail.com>
2022-12-29 23:52:04 -0700

    cusotm cmd bug fix

--- commands/custom.js
+++ commands/custom.js
@@ -6,7 +6,7 @@ const {
   Events,
 } = require('discord.js');
 
-const { createPromptembed } = require('../utils/embeds');
+const { createPromptEmbed } = require('../utils/embeds');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -26,7 +26,7 @@ module.exports = {
       'https://preview.redd.it/2y6iwo32z7581.png?width=640&crop=smart&auto=webp&s=34604909104f02d605772e714d001f60c8ed372c';
     // TODO: mimic api call for imageLink later obv
 
-    const embedding = createPromptembed(prompt, imageLink, imageLink);
+    const embedding = createPromptEmbed(prompt, imageLink, imageLink);
 
     const row = new ActionRowBuilder().addComponents(
       new ButtonBuilder()
