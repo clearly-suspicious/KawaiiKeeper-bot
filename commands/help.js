bce72ed1455cbe3e646f86a671195c9eab404b84
Vishal Sathyanarayana <vishal23dps@gmail.com>
2023-02-12 15:01:51 -0700

    added /help

---
+++ commands/help.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('Lists all available commands'),
+  async execute(interaction) {
+    let str;
+    const commandFiles = fs
+      .readdirSync('./commands')
+      .filter((file) => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+      const command = require(`./${file}`);
+      str += `Name: ${command.data.name}, Description: ${command.data.description} \n`;
+    }
+
+    return interaction.reply({
+      content: str,
+      ephemeral: true,
+    });
+  },
+};
